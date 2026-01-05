e0cbc27b6dd910fedf10bb121c56293f50daed19
piaomanor <piaomanor@gmail.com>
2026-01-05 11:13:04 +0800

    init

---
+++ OpenWrt-part1.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: OpenWrt-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# upgrade gcc11
+# sudo apt-get update && sudo apt-get upgrade -y
+# sudo apt-get install software-properties-common -y
+# sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+# sudo apt-get update
+# sudo apt-get install gcc-11 g++-11 -y
+# sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 60 --slave /usr/bin/g++ g++ /usr/bin/g++-11
+# sudo update-alternatives --config gcc
+# gcc --version
+
+# 自定义函数
+addFeeds(){
+  if [ $# == 2 ];then
+    echo src-git $1 $2 >> feeds.conf.default
+  fi
+}
+
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+
+
+# Uncomment a feed source
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+addFeeds custom https://github.com/kenzok8/openwrt-packages.git
+addFeeds small https://github.com/kenzok8/small.git
+
+# 支持 turboacc
+# 不带 shortcut-fe
+curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
+# 带  不带 shortcut-fe
+# curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+# shortcut-fe
+git_sparse_clone package https://github.com/chenmozhijin/turboacc shortcut-fe
+
+# 关机
+git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
+
+# 应用过滤
+git clone -b v6.1.3  https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+
+# 钉钉推送
+git clone --depth=1 https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
+
+# 端口转发 A luci app of socat for nftables
+# git clone --depth=1 https://github.com/WROIATE/luci-app-socat package/luci-app-socat
+git_sparse_clone main https://github.com/kenzok8/small-package luci-app-socat 
+
+# 定时重启
+git clone https://github.com/zxl78585/luci-app-autoreboot.git package/luci-app-autoreboot
+
+# 磁盘管理
+git clone --depth=1 https://github.com/lisaac/luci-app-diskman package/luci-app-diskman
+
+# 温度插件
+git clone --depth=1 https://github.com/gSpotx2f/luci-app-temp-status package/luci-app-temp-status
+
+# 带宽监控+在线设备，相互依赖
+git_sparse_clone main https://github.com/kenzok8/small-package luci-app-wrtbwmon wrtbwmon luci-app-onliner
+
+# usb打印+网络唤醒Plus
+git_sparse_clone main https://github.com/kenzok8/small-package luci-app-usb-printer luci-app-wolplus
+
+# kms
+git_sparse_clone master https://github.com/DokiDuck/luci-app-vlmcsd luci-app-vlmcsd vlmcsd
+
+
+# natmapt
+git clone --depth 1 --branch master --single-branch --no-checkout https://github.com/muink/openwrt-stuntman.git package/stuntman
+pushd package/stuntman
+umask 022
+git checkout
+popd
+
+git clone --depth 1 --branch master --single-branch --no-checkout https://github.com/muink/luci-app-natmapt.git package/luci-app-natmapt
+pushd package/luci-app-natmapt
+umask 022
+git checkout
+popd
+
+git clone --depth 1 --branch master --single-branch --no-checkout https://github.com/muink/openwrt-natmapt.git package/natmapt
+pushd package/natmapt
+umask 022
+git checkout
+popd
+
+git_sparse_clone main https://github.com/kenzok8/small-package dns2socks ipt2socks microsocks
+
+# 
+./scripts/feeds update -a
+# remove 
+rm -rf feeds/small/{luci-app-bypass,luci-app-ssr-plus}
--- README.md
+++ README.md
@@ -1,2 +1,121 @@
-# openwrt-vm
-iso vid root.tar.gz vm openwrt 
+# openwrt-vm docker 
+
+## **docker openwrt 旁路由配置指南 支持 公网 ipv6**
+
+<font color=red> ⛔ **(注意 由于 使用的是 docker macvlan 模式，宿主机 将无法访问 到macvlan，也就是说 宿主机 无法访问到 容器)**</font>
+
+### **宿主机网络相关配置**
+
+```
+# 开启网卡混杂模式(需要网线连接的网口，wifi无法使用混杂模式) 
+# enp0s31f6 网卡名称(根据自身电脑网口名称来) ifconfig or ip addr
+sudo ip link set enp1s0 promisc on
+
+# docker 开启 ipv6 支持 ip6tables = true
+vim /etc/docker/daemon.json
+
+{
+  "data-root": "/home/username/docker-root",
+  "experimental": true,
+  "ip6tables": true
+}
+
+# 重启 docker
+sudo systemctl restart docker
+```
+
+### **创建 docker macvlan 虚拟网卡**
+
+```
+# subnet、gateway 根据 局域网环境来填写
+# 此处的 ipv6 网关和 
+docker network create -d macvlan \
+--subnet=192.168.2.0/24 \
+--gateway=192.168.2.1 \
+--ipv6 \
+--subnet=fddb:b748:79d::/64 \
+--gateway=fddb:b748:79d::1 \
+-o parent=enp1s0 macnet
+```
+
+### **部署 openwrt 容器**
+
+```
+# 导入 容器到 docker 镜像
+docker import openwrt-x86-64-generic-rootfs.tar.gz openwrt:vm
+
+# 启动 openwrt 容器
+docker run -dit --name=vm \
+--restart=always  \
+--hostname=9847714a2b0a \
+--ip=192.168.2.3 \
+--network=macnet \
+--privileged \
+openwrt:vm /sbin/init
+```
+
+### **修改openwt 默认的 network 配置 支持 ipv6**
+
+```
+# 配置容器 lan
+# ipaddr=运行容器时指定的ip  
+# gateway=局域网路由器网关 
+# dns=局域网dns解析(默认一般都是 gateway 地址) 
+vim  /etc/config/network
+
+config interface 'lan'
+        option proto 'static'
+        option ipaddr '192.168.2.3'
+        option netmask '255.255.255.0'
+        option gateway '192.168.2.1'
+        option dns '192.168.2.1'
+        option ip6assign '64'
+        option device 'br-lan'
+
+config interface 'LAN6'
+        option proto 'dhcpv6'
+        option reqaddress 'try'
+        option reqprefix 'auto'
+        option device '@lan'
+
+config device
+        option name 'br-lan'
+        option type 'bridge'
+        list ports 'eth0'
+```
+
+### **openwrt 在 docker 有些服务不会自启，或者自启失败，需要命令执行重启操作**
+
+```
+# 编辑 /etc/rc.local 将需要自启动的 服务 写入 自启脚本
+vim /etc/rc.local
+/usr/sbin/uhttpd -p 80 -h /www
+/etc/init.d/pushbot restart
+/etc/init.d/passxxxx restart
+/etc/init.d/passxxxx_server restart
+/etc/init.d/v2ray_server restart
+exit 0
+```
+
+### **开启 ipv6 支持(编辑完之后重启容器)**
+
+[**此部分教程需要感谢这篇求助帖 来自网络**](https://meta.appinn.net/t/topic/46007)
+
+```
+# 编辑 /etc/sysctl.conf
+vim /etc/sysctl.conf
+net.ipv6.conf.all.disable_ipv6=0
+net.ipv6.conf.default.disable_ipv6=0
+net.ipv6.conf.default.accept_ra=2
+net.ipv6.conf.all.accept_ra=2
+```
+
+# 设置默认主题
+
+```shell
+uci set luci.main.mediaurlbase='/luci-static/argon'
+
+uci set luci.main.mediaurlbase='/luci-static/bootstrap'
+
+uci commit luci
+```
