79b5a393c98a0ae6ad9c5f3201c7854c612872ed
Nova <piaomanor@gmail.com>
2026-01-20 23:24:21 +0800

    Add Passwall feeds and clean up packages
    Added Passwall package feeds and removed unnecessary packages.

--- OpenWrt-part1.sh
+++ OpenWrt-part1.sh
@@ -43,6 +43,9 @@ function git_sparse_clone() {
 addFeeds custom https://github.com/kenzok8/openwrt-packages.git
 addFeeds small https://github.com/kenzok8/small.git
 
+src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main
+src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main
+
 # 支持 turboacc
 # 不带 shortcut-fe
 curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
@@ -106,6 +109,11 @@ git_sparse_clone main https://github.com/kenzok8/small-package dns2socks ipt2soc
 
 # 
 ./scripts/feeds update -a
+
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/utils/v2dat
+
 # remove 
 ls -lh feeds/small/
 rm -rf feeds/small/{luci-app-bypass,luci-app-ssr-plus}
